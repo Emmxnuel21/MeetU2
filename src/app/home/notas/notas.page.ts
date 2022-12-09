244142d64ff0f7a21d4a72d4addae9595f644585
Emmxnuel21 <em.orellana@duocuc.cl>
2022-12-08 23:50:40 -0300

    proyecto terminado con notas ok

--- src/app/home/notas/notas.page.ts
+++ src/app/home/notas/notas.page.ts
@@ -9,11 +9,34 @@ import { DbserviceService } from 'src/app/services/dbservice.service';
   styleUrls: ['./notas.page.scss'],
 })
 export class NotasPage implements OnInit {
-
+  
+  notas: any = [
+    {
+      titulo: "Titulo de la Nota",
+      texto: "Texto de la nota que quiero que salga en el cuerpo del item"
+    }
+  ]
   constructor(private servicioBD: DbserviceService, public router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(){
+    //this.servicioBD.presentAlert("1");
+    this.servicioBD.dbState().subscribe((res) =>{
+      //this.servicioBD.presentAlert("2");
+      if(res){
+        //this.servicioBD.presentAlert("3");
+        this.servicioBD.fetchNotas().subscribe(item =>{
+          this.notas = item;
+        })
+      }
+      //this.servicioBD.presentAlert("4");
+    });
   }
+
+  getItem($event) {
+    const valor = $event.target.value; 
+    console.log('valor del control: ' + valor);
+  }
+
   agregar() {
   }
 
